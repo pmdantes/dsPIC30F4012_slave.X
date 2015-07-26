8e44fb02e402ec35f42e0c74e79ee195b6334791
pmdantes <pmdantes@gmail.com>
2015-07-25 19:12:38 -0700

    Fixed PWM noise
    PID control updated. Fixes:
    Changed controller data type to signed integer and float.
    Changed conversion to PWM by using maximum error count instead of
    changing variables (i.e. degPOT)
    Hard coded saturation point values

--- main_4012_slave.c
+++ main_4012_slave.c
@@ -17,7 +17,6 @@
 #define PIC_HAPTIC      (PIC_ID+3)
 #define SLAVE_INDATA    0//(PIC_ID-4)
 #define MASTER_INDATA   (PIC_ID-1)
-#define MAX_PID_ERROR   1000.0
 
 // IO definitions
 #define LEDRED  LATEbits.LATE3 // RED
@@ -54,7 +53,7 @@
 
 // CAN bit timing
 #define FOSC        20000000 // 7.37MHz
-#define FCY         FOSC/4
+#define FCY         FOSC.0/4.0
 #define BITRATE     1000000  // 100kbps
 #define NTQ         16      // Amount of Tq per bit time
 #define BRP_VAL     (((4*FCY)/(2*NTQ*BITRATE))-1) // refer to pg. 693 of Family Reference
@@ -89,11 +88,14 @@
 #define PID_TD  0
 #define PID_TS  10
 #define PID_N   10
+#define MAX_PID_ERROR 1000.0
+#define CW   0
+#define CCW  1
 
 // Define PWM constants
-#define PWM_FREQUENCY       10000
+#define PWM_FREQUENCY       10000.0
 #define PWM_PRESCALER       0       // 0=1:1 1=1:4 2=1:16 3=1:64
-#define PWM_COUNTS_PERIOD   (FCY/PWM_FREQUENCY)-1   // 16kHz for FRC
+#define PWM_COUNTS_PERIOD   (5000000.0/10000.0)-1.0   // 16kHz for FRC
 
 // pid_t type
 typedef struct {
@@ -115,11 +117,12 @@ unsigned int C1INTFtest, RX0IFtest, whileLooptest = 0;
 unsigned int motorState = INITIALIZE;
 unsigned int ADCValue0, ADCValue1 = 0;
 unsigned int pwmOUT[2] = {0, 0};
+//unsigned int pwmOUTCW
+//unsigned int pwmOUTCCW
 float degPOT = 0.0;
 float degMTR = 0.0;
 unsigned int targetPos = 12000;
-float max_error = 1/MAX_PID_ERROR;
-unsigned int max_period = PWM_COUNTS_PERIOD;
+float max_error = 1.0/MAX_PID_ERROR;
 
 
 void InitCan(void) {
@@ -301,7 +304,7 @@ void InitTmr1(void)
    T1CONbits.TGATE = 0;     // Gated timer accumulation disabled
    T1CONbits.TCS = 0;       // Use Tcy as source clock
    T1CONbits.TCKPS = 0;     // Tcy/1 as input clock
-   PR1 = 50000;             // Interrupt period = 10ms
+   PR1 = 5000;             // Interrupt period = 10ms
    IFS0bits.T1IF = 0;       // Clear timer 1 interrupt flag
    IEC0bits.T1IE = 1;       // Enable timer 1 interrupts
    IPC0bits.T1IP = 7;       // Enable timer 1 interrupts
@@ -338,40 +341,37 @@ void InitPid(pid_t *p, float kp, float kd, float ki, float T, unsigned short N, 
 
 void CalcPid(pid_t *mypid, float degPOT, float degMTR)
 {
-    float pidOutDutyCycle;
-//    float test;
-//    unsigned int loadtest;
+    volatile float pidOutDutyCycle;
+    volatile int target = (int)targetPos;
+    volatile int motorPos = (int)POSCNT;
+    volatile float error = 0.0;
 
 //    mypid->y = degMTR;
-//    mypid->e = degPOT-degMTR;
-    mypid->e = (float) (degPOT - degMTR);
-//
+    error = (float) (target - motorPos);
+
 //    mypid->i = mypid->e+mypid->elast; // accumulated error
 ////    mypid->d = (mypid->T*mypid->dlast)/mypid->N + (mypid->Kd*mypid->T)*(mypid->y-mypid->ylast)/(mypid->T+(mypid->T/mypid->N));
 //    mypid->d = mypid->e-mypid->elast;
-    mypid->u = mypid->Kp*mypid->e;//+mypid->Kd*mypid->d+mypid->Ki*mypid->i;
+    mypid->u = mypid->Kp*error;//+mypid->Kd*mypid->d+mypid->Ki*mypid->i;
 
-//    pidOutDutyCycle = (float) ((mypid->u/degPOT)*2*PWM_COUNTS_PERIOD);
-    pidOutDutyCycle = (mypid->u*max_error)*2.0*PWM_COUNTS_PERIOD;
-//    test = (FCY/PWM_FREQUENCY)-1;
-//    loadtest=(unsigned int)test;
+//    pidOutDutyCycle = (float) ((mypid->u/degPOT)*2.0*PWM_COUNTS_PERIOD);
+    pidOutDutyCycle = (float) (mypid->u*0.1);
 
-
-    if (pidOutDutyCycle >= 2.0*PWM_COUNTS_PERIOD){
-        pwmOUT[0] = (unsigned int) (2.0*PWM_COUNTS_PERIOD);
-        pwmOUT[1] = 0;
+    if (pidOutDutyCycle >= 998.0){
+        pwmOUT[CW] = 997;
+        pwmOUT[CCW] = 0;
     }
-    else if (pidOutDutyCycle <= -(2.0*PWM_COUNTS_PERIOD)){
-        pwmOUT[0] = 0;
-        pwmOUT[1] = (unsigned int) (2.0*PWM_COUNTS_PERIOD);
+    else if (pidOutDutyCycle <= -998.0){
+        pwmOUT[CW] = 0;
+        pwmOUT[CCW] = 997;
     }
     else if (pidOutDutyCycle <0){
-        pwmOUT[0] = 0;
-        pwmOUT[1] = (unsigned int)((-1.0)*pidOutDutyCycle);
+        pwmOUT[CW] = 0;
+        pwmOUT[CCW] = (unsigned int)((-1.0)*pidOutDutyCycle);
     }
     else{
-        pwmOUT[0] = (unsigned int)(pidOutDutyCycle);
-        pwmOUT[1] = 0;
+        pwmOUT[CW] = (unsigned int)(pidOutDutyCycle);
+        pwmOUT[CCW] = 0;
     }
     return;
 }
@@ -400,7 +400,7 @@ int main() {
     TRISYLW = 0; // PORTE output
     TRISGRN = 0;
 
-    
+
 
     while (1) {
 
@@ -436,8 +436,8 @@ int main() {
             case SEND_HOME:
                 // run at 50% duty cycle until current value is high
                 // high current value = end of reach
-                pwmOUT[0] = 0;
-                pwmOUT[1] = PWM_COUNTS_PERIOD/2; // run CCW at 25% duty cycle
+                pwmOUT[CW] = 0;
+                pwmOUT[CCW] = PWM_COUNTS_PERIOD/2; // run CCW at 25% duty cycle
 //                if (ADCValue0 >= 70){
 //                    pwmOUT[0] = 0;
 //                    pwmOUT[1] = 0;
@@ -458,8 +458,8 @@ int main() {
 
                 C1TX0B4 = PIC_ID;
                 C1TX0B1 = POSCNT;
-                C1TX0B2 = pwmOUT[0];
-                C1TX0B3 = pwmOUT[1];
+                C1TX0B2 = pwmOUT[CW];
+                C1TX0B3 = pwmOUT[CCW];
                 C1TX0CONbits.TXREQ = 1;
                 while (C1TX0CONbits.TXREQ != 0);
                 break;
@@ -540,13 +540,12 @@ void __attribute__((interrupt, no_auto_psv)) _INT0Interrupt(void) {
 
 void __attribute__((interrupt, no_auto_psv)) _C1Interrupt(void) {
     IFS1bits.C1IF = 0; // Clear interrupt flag
+
     if (C1INTFbits.TX0IF) {
         C1INTFbits.TX0IF = 0;
     }
 
     if (C1INTFbits.RX0IF) {
-//        LEDRED = 1;
-
         C1INTFbits.RX0IF = 0;
 
         InData0[0] = C1RX0B1;
@@ -566,7 +565,6 @@ void __attribute__((interrupt, no_auto_psv)) _C1Interrupt(void) {
         InData1[3] = C1RX1B4;
         C1RX1CONbits.RXFUL = 0;
     }
-//    LEDRED = 0;
 }
 
 void __attribute__((interrupt, no_auto_psv)) _U1TXInterrupt(void) {
@@ -580,18 +578,15 @@ void __attribute__((interrupt, no_auto_psv)) _U1RXInterrupt(void) {
 void __attribute__((interrupt, no_auto_psv)) _T1Interrupt(void)
 {
     IFS0bits.T1IF = 0;   // Clear timer 1 interrupt flag
+
     // Read and convert ADC value and encoder position to degrees
-    LEDRED = 1;
-    degMTR = (float) POSCNT;
-    degPOT = (float) targetPos;
-//    degPOT = (float) 20000;
-//    degMTR = (float) (POSCNT * 0.09); // motor position
-//    degPOT = (float) (targetPos * 0.09); // target position with motor encoder input
+    degMTR = (float) (POSCNT); // motor position
+//    degPOT = (float) (12500); // target position with motor encoder input
+    degPOT = (float) (targetPos); // target position with motor encoder input
     CalcPid(&mypid, degPOT, degMTR);
     // Update PID variables
-//    UpdatePid(&mypid);
-    LEDRED = 0;
-    
+    UpdatePid(&mypid);
+
 //    PositionCalculation();
 //    Speed = AngPos[0] - AngPos[1];
 //    if (Speed >= 0)
